ce170ce0c1799d7b71cacb8d1d7b38e392ee8d1d
Britt <brittcagnina@gmail.com>
2013-10-28 17:57:52 -0500

    first commit

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,40 @@
+class MicropostsController < ApplicationController
+  before_action :signed_in_user
+  before_action :correct_user,   only: :destroy
+  
+  # GET /microposts/new
+  def new
+    @micropost = Micropost.new
+  end
+
+  # POST /microposts
+  # POST /microposts.json
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      @feed_items = []
+      render 'static_pages/home'
+    end
+  end
+
+  # DELETE /microposts/1
+  # DELETE /microposts/1.json
+  def destroy
+    @micropost.destroy
+    redirect_to root_url
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
+end
---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,12 @@
+module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Default Title"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title}"
+    end
+  end
+end
